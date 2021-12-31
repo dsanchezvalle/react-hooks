e9e3495b7c60b8e99f50bbc96f93bfe003342eb4
David Sanchez <davsanchez@sofi.org>
2021-12-31 17:57:36 -0300

    task with useCallback

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
@@ -12,12 +12,13 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 //import { Layout } from './components/05-useLayoutEffect/Layout';
 //import { Memorize } from './components/06-memos/Memorize';
 //import { MemoHook } from './components/06-memos/MemoHook';
+//import { CallbackHook } from './components/06-memos/CallbackHook';
 //import { HookApp } from './HookApp';
 
 
 ReactDOM.render(
   
-    < CallbackHook />,
+    <Padre />,
   document.getElementById('root')
 );
 
