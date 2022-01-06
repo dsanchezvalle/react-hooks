6de259eae0180cca6c208cb1534caaae377a6295
David Sanchez <davsanchez@sofi.org>
2022-01-06 18:33:52 -0300

    include routes

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -3,7 +3,11 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Redirect,
+    Navigate
   } from 'react-router-dom';
+  import { NavBar } from './NavBar';
+
 import { AboutScreen } from './AboutScreen';
 import { HomeScreen } from './HomeScreen';
 import { LoginScreen } from './LoginScreen';
@@ -13,10 +17,13 @@ export const AppRouter = () => {
     return (
         <Router>
             <div>
+                <NavBar />
                 <Routes>
                     <Route exact path="/about" element={<AboutScreen />} />
                     <Route exact path="/login" element={<LoginScreen />} />
                     <Route exact path="/" element={<HomeScreen />} />
+                    {/* <Route path="*" element={<HomeScreen />} /> */}
+                    <Route path="*" element={<Navigate to="./" />} />
                 </Routes>
             </div>
         </Router>
